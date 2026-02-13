5613366f9c1fb1844449bf9e22f9c6546d241c6a
Chakhit19 <mrchakhit.k@ku.th>
2026-02-13 12:37:24 +0700

    get post path put delete

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,75 @@
-//ทำการimportโมดูลhttp
-const http = require('http');
-const host = 'localhost';
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
 const port = 8000;
 
-//กำหนดค่าserver
-const reqestListener = function (req, res) {
-    res.writeHead(200);
-    res.end('Hello, World!');
-}
+let users =[];
+let counter = 1;
 
-//Run server
-const server = http.createServer(reqestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+
+//path: = GET /users
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+//path: = POST /user
+app.post('/user',(req,res) => {
+    let user = req.body;
+    user.id = counter
+    counter += 1;
+
+    users.push(user);
+    res.json({
+    message: 'user added successfully',
+    user: user
+    });
+});
+// path: = PUT /user/:id
+app.patch('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let updateUser = req.body;
+    
+    // หา user ที่จาก id ที่ส่งมา
+    let selectedIndex = users.findIndex(user => user.id == id);
+
+    if (selectedIndex === -1) {
+        return res.status(404).json({
+            message: "User not found"
+        });
+    }
+
+    if (updateUser.firstname) {
+        users[selectedIndex].firstname = updateUser.firstname;
+    }
+
+    if (updateUser.lastname) {
+        users[selectedIndex].lastname = updateUser.lastname;
+    }
+
+    res.json({
+        message: 'User updated successfully',
+        user: users[selectedIndex]
+    });
+    // ส่ง users ที่อัพเดตแล้วกลับไป
+});
+
+app.delete('/user/:id',(req,res) => {
+    let id = req.params.id;
+    // หา index จาก id  ที่ต้องการลบ
+    let selectedIndex = users.findIndex(user => user.id == id);
+    
+
+   //ลบuser ออกจาก users
+   users.splice(selectedIndex, 1);
+   res.json({
+            message:'user deleted successfully',
+            indexDelete: selectedIndex
+   });
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
